d1af9e4abdafcf8f2cad5e8ba87c1f3717f6185f
divipriyanka <159677474+divipriyanka@users.noreply.github.com>
2025-02-05 23:06:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import difflib
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 movie_name = input(' Enter your favorite movie name: ')

ef0f81526e322101e20c14255ad2d93a79d548bf
divipriyanka <159677474+divipriyanka@users.noreply.github.com>
2025-02-10 00:18:32 -0500

    Add files via upload

--- MovieRecommenderApp.py
+++ MovieRecommenderApp.py
@@ -1,34 +1,55 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Feb  9 16:57:22 2025
+Created on Sun Feb  9 22:16:59 2025
 
 @author: priya
 """
 
+import streamlit as st
 import numpy as np
 import pickle
-import streamlit as st
 import difflib
-import pandas as pd
-
-# Load your precomputed movie data and similarity matrix
-movies_data = pickle.load(open('movies_data.pkl', 'rb'))
-similarity = pickle.load(open('similarity.pkl', 'rb'))
+import pandas as pd    
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity    
 
 def movie_recommender_system(movie_name):
+    st.title("Movie Based Recommendation System")
+
+    # Load data
+    movies_data = pd.read_csv("movies.csv")
+    
+    #selecting the relevant features for recommendation
+    selected_features = ['genres','keywords','tagline','cast','director']
+    print(selected_features)
+    
+    #replacing the null values with null string
+
+    for feature in selected_features :
+        movies_data[feature] = movies_data[feature].fillna('')    
+        
+    # Combining all the 5 selected features
+    combined_features = movies_data['genres']+' '+movies_data['keywords']+' '+movies_data['tagline']+' '+movies_data['cast']+' '+movies_data['director']
+    
+    vectorizer = TfidfVectorizer()
+    feature_vectors = vectorizer.fit_transform(combined_features)
+    similarity = cosine_similarity(feature_vectors)
     list_of_all_titles = movies_data['title'].tolist()
     find_close_match = difflib.get_close_matches(movie_name, list_of_all_titles)
 
     if not find_close_match:
         return ["No close match found. Please try another movie."]
 
+    
     close_match = find_close_match[0]
-    index_of_the_movie = index_of_the_movie = movies_data[movies_data.title == close_match]['mID'].values[0]
+    index_of_the_movie = movies_data[movies_data.title == close_match]['index'].values[0]
     similarity_score = list(enumerate(similarity[index_of_the_movie]))
-    sorted_similar_movies = sorted(similarity_score, key=lambda x: x[1], reverse=True)
+    sorted_similar_movies = sorted(similarity_score, key = lambda x:x[1], reverse = True)
+    print('Movies suggested for you : \n')
+
 
     recommendations = []
-    for i, movie in enumerate(sorted_similar_movies[1:11], start=1):  # Skip first because it's the input movie itself
+    for i, movie in enumerate(sorted_similar_movies[1:31], start=1):  
         index = movie[0]
         recommended_movie = movies_data[movies_data.index == index]['title'].values[0]
         recommendations.append(f"{i}. {recommended_movie}")
